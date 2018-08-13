85ce654cc37d1d69ed22e843c582d91623a3aec8
HackFisher <hackfisher@gmail.com>
2018-08-13 18:41:13 +0800

    Add resouce tokens of gold,wood, water, fire, silicon

---
+++ src/WOOD.sol
@@ -0,0 +1,180 @@
+pragma solidity ^0.4.23;
+
+import "ds-token/token.sol";
+import './ERC223ReceivingContract.sol';
+import './TokenController.sol';
+import './Controlled.sol';
+import './ApproveAndCallFallBack.sol';
+import './ERC223.sol';
+
+contract WOOD is DSToken("WOOD"), ERC223, Controlled {
+
+    constructor() public {
+        setName("Evolution Land Wood");
+    }
+
+    /// @notice Send `_amount` tokens to `_to` from `_from` on the condition it
+    ///  is approved by `_from`
+    /// @param _from The address holding the tokens being transferred
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be transferred
+    /// @return True if the transfer was successful
+    function transferFrom(address _from, address _to, uint256 _amount
+    ) public returns (bool success) {
+        // Alerts the token controller of the transfer
+        if (isContract(controller)) {
+            if (!TokenController(controller).onTransfer(_from, _to, _amount))
+               throw;
+        }
+
+        success = super.transferFrom(_from, _to, _amount);
+    }
+
+    /*
+     * ERC 223
+     * Added support for the ERC 223 "tokenFallback" method in a "transfer" function with a payload.
+     */
+    function transferFrom(address _from, address _to, uint256 _amount, bytes _data)
+        public
+        returns (bool success)
+    {
+        // Alerts the token controller of the transfer
+        if (isContract(controller)) {
+            if (!TokenController(controller).onTransfer(_from, _to, _amount))
+               throw;
+        }
+
+        require(super.transferFrom(_from, _to, _amount));
+
+        if (isContract(_to)) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(_from, _amount, _data);
+        }
+
+        emit ERC223Transfer(_from, _to, _amount, _data);
+
+        return true;
+    }
+
+    /*
+     * ERC 223
+     * Added support for the ERC 223 "tokenFallback" method in a "transfer" function with a payload.
+     * https://github.com/ethereum/EIPs/issues/223
+     * function transfer(address _to, uint256 _value, bytes _data) public returns (bool success);
+     */
+    /// @notice Send `_value` tokens to `_to` from `msg.sender` and trigger
+    /// tokenFallback if sender is a contract.
+    /// @dev Function that is called when a user or another contract wants to transfer funds.
+    /// @param _to Address of token receiver.
+    /// @param _amount Number of tokens to transfer.
+    /// @param _data Data to be sent to tokenFallback
+    /// @return Returns success of function call.
+    function transfer(
+        address _to,
+        uint256 _amount,
+        bytes _data)
+        public
+        returns (bool success)
+    {
+        return transferFrom(msg.sender, _to, _amount, _data);
+    }
+
+    /// @notice `msg.sender` approves `_spender` to spend `_amount` tokens on
+    ///  its behalf. This is a modified version of the ERC20 approve function
+    ///  to be a little bit safer
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _amount The amount of tokens to be approved for transfer
+    /// @return True if the approval was successful
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        // Alerts the token controller of the approve function call
+        if (isContract(controller)) {
+            if (!TokenController(controller).onApprove(msg.sender, _spender, _amount))
+                throw;
+        }
+        
+        return super.approve(_spender, _amount);
+    }
+
+    function mint(address _guy, uint _wad) auth stoppable {
+        super.mint(_guy, _wad);
+
+        emit Transfer(0, _guy, _wad);
+    }
+    function burn(address _guy, uint _wad) auth stoppable {
+        super.burn(_guy, _wad);
+
+        emit Transfer(_guy, 0, _wad);
+    }
+
+    /// @notice `msg.sender` approves `_spender` to send `_amount` tokens on
+    ///  its behalf, and then a function is triggered in the contract that is
+    ///  being approved, `_spender`. This allows users to use their tokens to
+    ///  interact with contracts in one function call instead of two
+    /// @param _spender The address of the contract able to transfer the tokens
+    /// @param _amount The amount of tokens to be approved for transfer
+    /// @return True if the function call was successful
+    function approveAndCall(address _spender, uint256 _amount, bytes _extraData
+    ) returns (bool success) {
+        if (!approve(_spender, _amount)) throw;
+
+        ApproveAndCallFallBack(_spender).receiveApproval(
+            msg.sender,
+            _amount,
+            this,
+            _extraData
+        );
+
+        return true;
+    }
+
+    /// @dev Internal function to determine if an address is a contract
+    /// @param _addr The address being queried
+    /// @return True if `_addr` is a contract
+    function isContract(address _addr) constant internal returns(bool) {
+        uint size;
+        if (_addr == 0) return false;
+        assembly {
+            size := extcodesize(_addr)
+        }
+        return size>0;
+    }
+
+    /// @notice The fallback function: If the contract's controller has not been
+    ///  set to 0, then the `proxyPayment` method is called which relays the
+    ///  ether and creates tokens as described in the token controller contract
+    function ()  payable {
+        if (isContract(controller)) {
+            if (! TokenController(controller).proxyPayment.value(msg.value)(msg.sender))
+                throw;
+        } else {
+            throw;
+        }
+    }
+
+//////////
+// Safety Methods
+//////////
+
+    /// @notice This method can be used by the controller to extract mistakenly
+    ///  sent tokens to this contract.
+    /// @param _token The address of the token contract that you want to recover
+    ///  set to 0 in case you want to extract ether.
+    function claimTokens(address _token) onlyController {
+        if (_token == 0x0) {
+            controller.transfer(address(this).balance);
+            return;
+        }
+
+        ERC20 token = ERC20(_token);
+        uint balance = token.balanceOf(this);
+        token.transfer(controller, balance);
+
+        emit ClaimedTokens(_token, controller, balance);
+    }
+
+////////////////
+// Events
+////////////////
+
+    event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
+}
