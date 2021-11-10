389cfc2d5821dba4603ac1c18430ae04c332d4ee
echo <hujw77@gmail.com>
2021-11-10 16:05:29 +0800

    rename WCRING to WCRAB

--- src/WCRAB.sol
+++ src/WCRAB.sol
@@ -15,9 +15,9 @@
 
 pragma solidity >=0.4.23;
 
-contract WCRING {
-    string public name     = "Wrapped CRING";
-    string public symbol   = "WCRING";
+contract WCRAB {
+    string public name     = "Wrapped CRAB";
+    string public symbol   = "WCRAB";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
