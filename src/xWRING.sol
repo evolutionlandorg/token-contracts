05d2494e8cab19b669af17be2f3780745ab01217
echo <hujw77@gmail.com>
2022-11-01 11:23:53 +0800

    xWRING

--- src/xWRING.sol
+++ src/xWRING.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.23;
 
 import "ds-token/token.sol";
 
-contract xRING is DSToken("xWRING") {
+contract xWRING is DSToken("xWRING") {
 
     constructor() public {
         setName("Wrapped Ring[Darwinia Smart>(Test)");
